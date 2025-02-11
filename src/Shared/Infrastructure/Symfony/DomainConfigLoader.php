1e2f1a94aa19bdac667dc6bdf930bd5dde919a09
Arnaud P <arnaud@xpressive.io>
2025-02-11 22:46:27 +0100

    First draft domain config loader

---
+++ src/Shared/Infrastructure/Symfony/DomainConfigLoader.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Symfony;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * Component used to load Symfony services definition files from within domains directories.
+ *
+ * @see src/Kernel.php
+ */
+final class DomainConfigLoader
+{
+    public static function load(string $projectDir, LoaderInterface $loader): void
+    {
+        $files = self::recursiveGlob(sprintf('%s/src/**/services.{yaml,yml}', $projectDir), GLOB_BRACE);
+
+        array_walk($files, function (&$file) use ($loader) {
+            $loader->load($file);
+        });
+    }
+
+    private static function recursiveGlob(string $pattern, int $flags = 0): array
+    {
+        $files = glob($pattern, $flags);
+
+        foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR) as $directory) {
+            $files = array_merge($files, self::recursiveGlob($directory.'/'.basename($pattern), $flags));
+        }
+
+        return $files;
+    }
+}
---
+++ tests/Unit/Shared/Infrastructure/Symfony/DependencyInjection/DomainConfigLoaderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Shared\Infrastructure\Symfony\DependencyInjection;
+
+use App\Shared\Infrastructure\Symfony\DomainConfigLoader;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class DomainConfigLoaderTest extends TestCase
+{
+    public function testDomainConfigLoading(): void
+    {
+        $loaderMock = $this->createMock(LoaderInterface::class);
+        $loaderMock->expects($this->exactly(3))->method('load');
+
+        DomainConfigLoader::load(dirname(__DIR__, 5).'/.stubs/structure', $loaderMock);
+    }
+}
