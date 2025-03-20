49eb49a179504e9b36984454de2a689e5aa4e3d0
Arnaud P <arnaud@xpressive.io>
2025-03-20 22:55:38 +0100

    Added console command to create migration

---
+++ src/Shared/UI/Console/Command/CreateMigrationCommand.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\UI\Console\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(name: 'migrations:create', description: 'Create a new migration file')]
+final class CreateMigrationCommand extends AbstractCommand
+{
+    public function __construct(
+        private readonly string $migrationsPath
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('name', InputArgument::REQUIRED, 'The name of the migration');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io->title('Create new migration file ...');
+
+        $name = $input->getArgument('name');
+
+        // Format the name
+        $name = preg_replace('/[^a-z0-9_]+/', '_', strtolower($name));
+        $timestamp = date('YmdHis');
+        $filename = sprintf('%s_%s.php', $timestamp, $name);
+        $filepath = $this->migrationsPath . '/' . $filename;
+
+        $template = <<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+use App\Shared\Domain\Database\Migration;
+use App\Shared\Infrastructure\PDO\Connection;
+
+return new class implements Migration
+{
+    public function up(Connection $connection): void
+    {
+        $connection->exec('
+            -- Your SQL code here
+        ');
+    }
+
+    public function down(Connection $connection): void
+    {
+        $connection->exec('
+            -- Revert SQL code here
+        ');
+    }
+};
+PHP;
+
+        file_put_contents($filepath, $template);
+
+        $this->io->success(sprintf('Created migration: %s', $filename));
+
+        return Command::SUCCESS;
+    }
+}
---
+++ tests/Application/Shared/UI/Console/Command/CreateMigrationCommandTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Application\Shared\UI\Console\Command;
+
+use App\Shared\UI\Console\Command\CreateMigrationCommand;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Filesystem\Filesystem;
+
+class CreateMigrationCommandTest extends TestCase
+{
+    private string $testMigrationsDir;
+    private Filesystem $filesystem;
+
+    protected function setUp(): void
+    {
+        $this->filesystem = new Filesystem();
+        $this->testMigrationsDir = sys_get_temp_dir() . '/migrations_test_' . uniqid();
+        $this->filesystem->mkdir($this->testMigrationsDir);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->filesystem->remove($this->testMigrationsDir);
+    }
+
+    public function testCreatesNewMigrationFile(): void
+    {
+        $application = new Application();
+        $command = new CreateMigrationCommand($this->testMigrationsDir);
+        $application->add($command);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => 'migrations:create',
+            'name' => 'create_test_table',
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Created migration:', $output);
+
+        $files = glob($this->testMigrationsDir . '/*.php');
+        $this->assertCount(1, $files);
+
+        $filename = basename($files[0]);
+        $this->assertMatchesRegularExpression('/^\d{14}_create_test_table\.php$/', $filename);
+
+        $content = file_get_contents($files[0]);
+        $this->assertStringContainsString('use App\Shared\Domain\Database\Migration;', $content);
+        $this->assertStringContainsString('public function up(Connection $connection): void', $content);
+        $this->assertStringContainsString('public function down(Connection $connection): void', $content);
+    }
+
+    public function testSanitizesFilename(): void
+    {
+        $application = new Application();
+        $command = new CreateMigrationCommand($this->testMigrationsDir);
+        $application->add($command);
+
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => 'migrations:create',
+            'name' => 'Create User-Table with SPECIAL!@#$% chars',
+        ]);
+
+        $files = glob($this->testMigrationsDir . '/*.php');
+        $filename = basename($files[0]);
+
+        $this->assertMatchesRegularExpression('/^\d{14}_create_user_table_with_special_chars\.php$/', $filename);
+    }
+}
